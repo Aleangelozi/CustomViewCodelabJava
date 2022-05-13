95a6f61e9cbb4045debe6842e461523e2e50134a
aleangelozi <aleangelozi@gmail.com>
2022-05-13 06:43:35 -0300

    Initial commit

---
+++ app/src/main/java/com/aleangelozi/customviewcodelabjava/SimpleDrawingView.java
@@ -0,0 +1,48 @@
+package com.aleangelozi.customviewcodelabjava;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.List;
+
+public class SimpleDrawingView extends View {
+    // setup initial color
+    private final int paintColor = Color.BLACK;
+    // defines paint and canvas
+    private Paint drawPaint;
+    // Store circles to draw each time the user touches down
+    private List<Point> circlePoints;
+
+    public SimpleDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setupPaint();
+    }
+
+    // Let's draw three circles
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawCircle(50, 50, 20, drawPaint);
+        drawPaint.setColor(Color.GREEN);
+        canvas.drawCircle(50, 150, 20, drawPaint);
+        drawPaint.setColor(Color.BLUE);
+        canvas.drawCircle(50, 250, 20, drawPaint);
+    }
+
+    // Setup paint with color and stroke styles
+    private void setupPaint() {
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+}
